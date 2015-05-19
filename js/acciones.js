08327981edf39fd5d13f3882f7e3d4e0d22e63e0
AL34F13 <robertosev@live.com>
2015-05-18 19:18:47 -0500

    Two
    nada.

---
+++ js/acciones.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+
+
+$('#btnjugar').on('tap',function(){
+	$('#juego').show()
+	$('#naruto').play()
+$('#btninicio').on('tap',function(){
+	$('#inicio').show()
+$('#1').on('tap', function() {
+	
+	alert ("1");
+	
+}); 
+});
+});
+}); 
+});
+
+
