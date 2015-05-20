a48ab0a558f16ca221254d4fd35854b32b47ba46
AL34F13 <robertosev@live.com>
2015-05-19 19:17:25 -0500

    cuatro
    correcion

--- js/acciones.js
+++ js/acciones.js
@@ -1,20 +1,31 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
+//document.addEventListener("deviceready",function(){
 
-
+$('#btnhome').on('tap',function() {
+ $('#inicio').show()
+});
 $('#btnjugar').on('tap',function(){
 	$('#juego').show()
-	$('#naruto').play()
+	$('#naruto').play()});
 $('#btninicio').on('tap',function(){
-	$('#inicio').show()
-$('#1').on('tap', function() {
-	$('#listado').append("<p>1</p>")
+	$('#inicio').show()});	
+$('#uno').on('tap', function() {
+	$('#click').append('1')
+	}); 
+$('#dos').on('tap', function() {
+	$('#click').append('2')
+	}); 
+$('#tres').on('tap', function() {
+	$('#click').append('3')
+	}); 			
+$('#cuatro').on('tap', function() {
+	$('#click').append('4')
+	}); 
 	
-}); 
+
 });
-});
-}); 
-});
+//}); 
+
 
 
